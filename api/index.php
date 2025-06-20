3c08c252ae73d4da11dbde130b5d8dfb9e213741
naphatsornn <naphatsorn.ki@ku.th>
2025-06-20 14:56:18 +0700

    fix php syntax

--- api/index.php
+++ api/index.php
@@ -1,25 +1,3 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
-
-use Kreait\Firebase\Factory;
-
-// ดึงค่า config จาก Environment
-$configJson = getenv('FIREBASE_CONFIG_JSON');
-$config = json_decode($configJson, true);
-
-$firebase = (new Factory)
-    ->withServiceAccount($config)
-    ->createDatabase();
-
-$headers = getallheaders();
-$data = [
-    'authorization' => $headers['Authorization'] ?? '',
-    'tmn_token_type' => $headers['tmn-token-type'] ?? '',
-    'tmn_access_token' => $headers['tmn-access-token'] ?? '',
-    'user_agent' => $headers['User-Agent'] ?? '',
-    'timestamp' => date('Y-m-d H:i:s'),
-];
-
-$firebase->getReference('headers')->push($data);
-
 echo '✅ Data saved to Firebase!';
+echo "App is running.";
